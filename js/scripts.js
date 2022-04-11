059e5902d1b06a78f39001bbe0ef979f7438d525
Mika <60449239+mikarrega@users.noreply.github.com>
2022-04-10 21:11:07 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+const btnDropdown = document.querySelector('.js-btn-dropdown');
+const dropdown = document.querySelector('.js-dropdown')
+
+function openDropdown(event) {
+    event.preventDefault();
+
+    dropdown.classList.toggle('active');
+    console.log(dropdown)
+}
+
+btnDropdown.addEventListener('click', openDropdown)
+dropdown.addEventListener('mouseleave', openDropdown)
+
+const header = document.getElementById('js-header');
+
+function fixedMenu () {
+    if(window.pageYOffset > 80) {
+        header.classList.add('fixed-menu');
+    } else {
+        header.classList.remove('fixed-menu');
+    }
+}
+
+document.addEventListener('scroll', fixedMenu)
+
+const btnMenu = document.getElementById('js-btn-menu-mobile');
+const overlayMenu = document.querySelector(".js-overlay");
+
+function openMenuMobile() {
+    document.documentElement.classList.toggle('menu-opened')
+}
+
+btnMenu.addEventListener('click', openMenuMobile);
+overlayMenu.addEventListener('click', openMenuMobile);
